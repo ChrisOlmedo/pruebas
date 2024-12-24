502b1f128a9fe43d933337d8662e87e833e8f3b6
ChrisOlmedo <alejandro.olmedo14@gmail.com>
2024-12-24 00:12:40 -0600

    fe

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html',
+    },
+  }
 })
